71767ac98cbb2956974bc20fdf5f2d9e3c6b4224
Rainer Stropek <rainer@software-architects.at>
2026-02-09 11:31:50 +0100

    Adding methods to structs

--- playground/main.go
+++ playground/main.go
@@ -5,6 +5,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math"
 )
 
 type Point struct {
@@ -16,15 +17,37 @@ type Rect struct {
 	LeftUpper, RightLower Point
 }
 
+func (r Rect) Width() float32 {
+	return float32(math.Abs(float64(r.RightLower.X - r.LeftUpper.X)))
+}
+
+func (r Rect) Height() float32 {
+	return float32(math.Abs(float64(r.RightLower.Y - r.LeftUpper.Y)))
+}
+
+func (r Rect) Area() float32 {
+	return r.Width() * r.Height()
+}
+
+func (r *Rect) Enlarge(factor float32) {
+	r.RightLower.X = r.LeftUpper.X + factor * r.Width()
+	r.RightLower.Y = r.LeftUpper.Y + factor * r.Height()
+}
+
 type Circle struct {
 	Center Point
 	Radius float32
 }
 
+func (c Circle) Area() float32 {
+	return math.Pi * c.Radius * c.Radius
+}
+
 func main() {
 	r := Rect{LeftUpper: Point{X: 0, Y: 10}, RightLower: Point{X: 10, Y: 0}}
-	fmt.Println(r)
-
+	fmt.Printf("Width: %f, Height: %f, Area: %f\n", r.Width(), r.Height(), r.Area())
+	r.Enlarge(2)
+	fmt.Printf("Width: %f, Height: %f, Area: %f\n", r.Width(), r.Height(), r.Area())
 	c := Circle{Center: Point{X: 0, Y: 0}, Radius: 5}
 	fmt.Println(c)
 
