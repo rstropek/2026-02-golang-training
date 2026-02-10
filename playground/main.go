2b495f3b2fd9afbd8af56851117bba4128d545b7
Rainer Stropek <rainer@software-architects.at>
2026-02-10 08:46:37 +0100

    Div with panic and repl

---
+++ playground/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func div(a, b int) int {
+	if b == 0 {
+		panic("division by zero")
+	}
+	return a / b
+}
+
+func repl() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("a: ")
+		a, _ := reader.ReadString('\n')
+		fmt.Print("b: ")
+		b, _ := reader.ReadString('\n')
+		a = strings.TrimSpace(a)
+		b = strings.TrimSpace(b)
+		aInt, _ := strconv.Atoi(a)
+		bInt, _ := strconv.Atoi(b)
+		result := div(aInt, bInt)
+		fmt.Printf("result: %d\n", result)
+	}
+}
+
+func main() {
+	repl()
+}
