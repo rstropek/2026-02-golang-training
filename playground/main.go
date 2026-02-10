0c313baabc3b0ea9560f9c36ce72a53a9348166b
Rainer Stropek <rainer@software-architects.at>
2026-02-10 10:03:42 +0100

    Refactor doSomethingAsync to use a buffered channel, improving non-blocking behavior for initial result sending

--- playground/main.go
+++ playground/main.go
@@ -8,7 +8,7 @@ import (
 func doSomethingAsync(result chan string) {
 	defer close(result)
 	fmt.Println("Starting to do something")
-	result <- "Step 1"
+	result <- "Step 1" // No longer a blocking operation because the channel is buffered
 	fmt.Println("Step 1 done")
 	time.Sleep(1 * time.Second)
 	result <- "Step 2"
@@ -17,9 +17,9 @@ func doSomethingAsync(result chan string) {
 }
 
 func main() {
-	result := make(chan string)
+	result := make(chan string, 2)
 	go doSomethingAsync(result)
 	time.Sleep(10 * time.Second)
-	fmt.Println(<-result)
+	fmt.Println(<-result) // Blocking operation (wait for sender to send a value)
 	fmt.Println(<-result)
 }
