a04aac1425bd9f48c4fac8486d978687f72b7131
Rainer Stropek <rainer@software-architects.at>
2026-02-11 14:04:41 +0100

    Add healthcheck and basic server setup

--- hero-manager/cmd/api/main.go
+++ hero-manager/cmd/api/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log/slog"
+	"os"
+)
+
+// Stores configuration settings for the application
+type config struct {
+	port int    // The port number the server will listen on
+	env  string // The environment the application is running in (e.g., "dev", "prod")
+}
+
+type application struct {
+	config config
+	logger *slog.Logger
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	var cfg config
+
+	// Use the flag package to read command-line flags for port and environment
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "dev", "Environment (dev|prod)")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	err := app.serve()
+	if err != nil {
+		logger.Error("Error starting server", "error", err)
+		os.Exit(1)
+	}
 }
--- hero-manager/go.mod
+++ hero-manager/go.mod
@@ -1,3 +1,5 @@
 module github.com/rstropek/hero-manager
 
 go 1.25.7
+
+require github.com/julienschmidt/httprouter v1.3.0
